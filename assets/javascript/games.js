14ae4b7b9b4baf98ad262cd438452b5affbb8116
Samuel Mast <smast@Samuels-MacBook-Pro.local>
2018-01-20 15:28:17 -0700

    final game updates

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -3,22 +3,29 @@ var winCount = 0;
 
 var currentWord = "";
 var currentWordArray = [];
+var currentDisplayWord = ""
 var currentDisplay = [];
-var currentDisplayArray = [];
-var userGuess = 0;
+var userGuess = "";
 
-var guessesLeft = 10;
+var guessesLeft = 9;
 var guessesSoFar = [];
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	//list of choices for the word to guess (replace with real words when ready)
-var wordOptions = ["word", "sam", "mast", "test"];
+var wordOptions = ["dunk", "slam", "pass", "cut", "shoot", "crossover", "dribble", "foul", "layup", "jordan",  "nuggets", "bulls", "lakers", "celtics", "timberwolves", "kobe", "lebron", "warriors", "clippers", "alleyoop", "fadeaway", "mvp", "champion", "ring", "nike", "adidas",  ];
 
-//Funtions
+
+
+//Functions
 
 
 function winAdd(x) {
 	winCount++;
+	document.getElementById("winCount").textContent = winCount;
+}
+
+function winReset() {
+	winCount = 0;
 	document.getElementById("winCount").textContent = winCount;
 }
 
@@ -37,20 +44,27 @@ function wordSetup() {
 		currentDisplay[i] = "_";
 	}
 
-	currentDisplayArray = currentDisplay.join(" ");
-	document.getElementById("currentWord").textContent = currentDisplayArray;
+	document.getElementById("currentWord").textContent = currentDisplay.join(" ");
 }
 
 function refresh() {
-	guessesLeft = 10;
+	guessesLeft = 9;
 	document.getElementById("guessesLeft").textContent = guessesLeft;
 
 	guessesSoFar = [];
 	document.getElementById("guessesSoFar").textContent = guessesSoFar;
 
+	currentDisplay = [];
+	document.getElementById("currentWord").textContent = currentDisplay;
+
+	var image = document.getElementById("currentImage");
+	image.src = "assets/images/hangman-start.png";	
+
 }
 
-
+function joinWord() {
+	currentDisplayWord = currentDisplay.join("");
+}
 
 //Click/Guess Function
 
@@ -72,8 +86,12 @@ document.onkeyup = function(event) {
 			if (guessesLeft === 0) {
 				//game over
 				
-				alert("Game Over");
+				var image = document.getElementById("currentImage");
+				image.src = "assets/images/hangman-final-loss.png";
 
+				alert("Game Over, the word was '" + currentWord + "'");
+
+				winReset();
 				refresh();
 				wordSetup();
 
@@ -86,28 +104,59 @@ document.onkeyup = function(event) {
 			for (var i = 0; i < currentWordArray.length; i++) {
 
 				if (currentWordArray[i] === userGuess) {
-					currentDisplayArray[i] = userGuess;
-
+					currentDisplay[i] = userGuess;
 				}
 			}
-			console.log(currentDisplayArray);
-			document.getElementById("currentWord").textContent = currentDisplayArray;
+			console.log(currentDisplay);
+			document.getElementById("currentWord").textContent = currentDisplay.join(" ");
 
-
-
-			if (currentWordArray === currentDisplayArray) {
-				//win
-				alert("You win, the word was" + currentWord);
-				
-				winCount();
-
-				refresh();
-				wordSetup();
-
-			}
 		}
 	} else {
 		console.log("Not a Valid Guess");
+	}
+
+	joinWord();
+
+	if (currentWord == currentDisplayWord) {
+		//win
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-final-win.png";
+
+		alert("You win, the word was " + currentWord);
+				
+	
+		winAdd();
+		document.getElementById("currentWord").textContent = ("...");
+
+		refresh();
+		wordSetup();
+
+	}
+
+	if (guessesLeft === 8) {
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-1.png";
+	} else if (guessesLeft === 7) {
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-2.png";
+	} else if (guessesLeft === 6) {
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-3.png";
+	} else if (guessesLeft === 5) {
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-4.png";
+	} else if (guessesLeft === 4) {
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-5.png";
+	} else if (guessesLeft === 3) {
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-6.png";
+	} else if (guessesLeft === 2) {
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-7.png";
+	} else if (guessesLeft === 1) {
+		var image = document.getElementById("currentImage");
+		image.src = "assets/images/hangman-8.png";
 	}
 }
 
